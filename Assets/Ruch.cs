e575cc9064a02ce2b4ed138e325e6248c7e73e2a
Marcin Juranek <m.juranek@PGG.PL>
2020-04-08 11:09:29 +0200

    Rotation catapult

--- Assets/Ruch.cs
+++ Assets/Ruch.cs
@@ -10,7 +10,9 @@ public class Ruch : MonoBehaviour
     public GameObject target_position;
     public GameObject ball_position;
     public GameObject brake_object;
-    private GameObject arm_object;
+    public GameObject arm_object;
+    public GameObject left_arm_break;
+    public GameObject right_arm_break;
     public float Vr =  0f;
     // Start is called before the first frame update
     void Start()
@@ -19,19 +21,19 @@ public class Ruch : MonoBehaviour
         MainCamera = GameObject.Find("Main_Cam").GetComponent<Camera>();
         TargetCamera = GameObject.Find("Target_Cam").GetComponent<Camera>();
         BallCamera = GameObject.Find("Ball_Cam").GetComponent<Camera>();
-       // arm_object = GameObject.FindGameObjectWithTag("MainPartCatapult");
     }
 
     // Update is called once per frame
     void Update()
     {
         //change camera
-        SetEnabledCamera(KeyCode.C, false, true, false);
-        SetEnabledCamera(KeyCode.D, true, false, false);
-        SetEnabledCamera(KeyCode.B, false, false, true);
+        SetEnabledCamera(KeyCode.Alpha1, false, true, false);
+        SetEnabledCamera(KeyCode.Alpha2, true, false, false);
+        SetEnabledCamera(KeyCode.Alpha3, false, false, true);
         //Throw ball
         ThrowBall(KeyCode.Space,target_position.transform.position,ball_position.transform.position,1.0f);
-        //use keys arrow
+       
+        //use keys arrow to rotate
         RotationLeftORightGameObject(brake_object, KeyCode.LeftArrow, 0, -1, 0.1f);
         RotationLeftORightGameObject(brake_object, KeyCode.RightArrow, 0, 1, 0.1f);
     }
@@ -40,6 +42,11 @@ public class Ruch : MonoBehaviour
         if (Input.GetKey(key))
         {
             catapultBody.transform.Rotate(new Vector3(x_value, y_value), angle_value);  
+            arm_object.transform.Rotate(new Vector3(x_value, y_value), angle_value);
+            left_arm_break.transform.Rotate(new Vector3(x_value, y_value), angle_value);
+            right_arm_break.transform.Rotate(new Vector3(x_value, y_value), angle_value);
+
+            Debug.Log("kąt Wartość = " + arm_object.transform.rotation.w.ToString());
         }
     }
 
@@ -55,18 +62,26 @@ public class Ruch : MonoBehaviour
     private void ThrowBall(KeyCode key, Vector3 target, Vector3 origin, float time)
     {
         Vector3 Vo = CalculateVelocity(target, origin, time);
+
         if (Input.GetKeyDown(key))
         {
             //angularVelocity mierzy predkosc katowa, stopnie na sekune
             Vr = Vr == 0 ? -6.0f : -Vr;
-            GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, Vr);
-            //Debug.Log("Pozycja"
-            //    + " X: " + Vo.x
-            //    + " Y: " + Vo.y
-            //    + " Z: " + Vo.z);          
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, Vr);   
+        }
+        
+    }
+    private void MoveForwardObject(float speed, GameObject gameobject, KeyCode key)
+    {
+        if (Input.GetKeyDown(key))
+        {
+            float forwardSpeed = 1.0f;
+            Vector3 velocity = new Vector3(0f, 0f, forwardSpeed);
+            var rb = GetComponent<Rigidbody>();
+            rb.interpolation = RigidbodyInterpolation.Interpolate;
+            rb.MovePosition(transform.position + forwardSpeed * velocity * Time.fixedDeltaTime);
         }
     }
-
     IEnumerator stopParam()
     {
         yield return new WaitForSeconds(.3f);
