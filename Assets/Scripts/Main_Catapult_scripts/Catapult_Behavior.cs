7a0af28706fa9fede76cdf662616a370d72047b7
Marcin Juranek <m.juranek@PGG.PL>
2020-04-29 12:22:58 +0200

    Add new class Catapult_ThrowningBall, remove method from Catapult_Behavior, add interface  IThrowBall and method ThrowBall

--- Assets/Scripts/Main_Catapult_scripts/Catapult_Behavior.cs
+++ Assets/Scripts/Main_Catapult_scripts/Catapult_Behavior.cs
@@ -6,36 +6,37 @@ using UnityEngine;
 
 public class Catapult_Behavior : MonoBehaviour
 {
-    private Camera MainCamera, TargetCamera, BallCamera;
-    
     public GameObject target_position;
     public GameObject ball_gameObject;
     public GameObject brake_object;
-    public GameObject base_object;
+    public GameObject mainPartCatapult;
     public GameObject left_arm_break;
     public GameObject right_arm_break;
     public GameObject arm_object;
     public GameObject ground_target_position;
     public float Vr =  0f;
 
-   
     private Vector3 start_ball_position;
     private IEnabledCamera setCamera;
     private IRotationCatapult rotateCapult;
+    private IThrowBall throwBall;
     // Start is called before the first frame update
     public Catapult_Behavior()
     {
-           
+        //this.rotateCapult = new Catapult_TurnOver(base_object, left_arm_break, right_arm_break, brake_object);
+        //this.throwBall = new Catapult_ThrowningBall(Vr, ball_gameObject);
     }
     void Start()
     {
         StartCoroutine(stopParam());
-        MainCamera = GameObject.Find("Main_Cam").GetComponent<Camera>();
-        TargetCamera = GameObject.Find("Target_Cam").GetComponent<Camera>();
-        BallCamera = GameObject.Find("Ball_Cam").GetComponent<Camera>();
-        this.setCamera = new Camera_Definitions(MainCamera,TargetCamera,BallCamera);
+
+        this.setCamera = new Camera_Definitions(
+            GameObject.Find("Main_Cam").GetComponent<Camera>(),
+            GameObject.Find("Target_Cam").GetComponent<Camera>(), 
+            GameObject.Find("Ball_Cam").GetComponent<Camera>());
         start_ball_position = ball_gameObject.transform.position;
-        this.rotateCapult = new Catapult_TurnOver(base_object, left_arm_break, right_arm_break, brake_object);
+        this.rotateCapult = new Catapult_TurnOver(mainPartCatapult, left_arm_break, right_arm_break, brake_object);
+        this.throwBall = new Catapult_ThrowningBall(Vr, ball_gameObject, mainPartCatapult);
     }
 
     // Update is called once per frame
@@ -47,87 +48,20 @@ public class Catapult_Behavior : MonoBehaviour
         setCamera.SetEnabledCamera(KeyCode.Alpha3, false, false, true);
 
         //Throw ball
-        ThrowBall(KeyCode.Space,target_position.transform.position, ball_gameObject.transform.position,1.0f, ball_gameObject);
-        //MoveForwardObject(15.0f, ball_gameObject, KeyCode.Space);
-
-        //use keys arrow to rotate
-        rotateCapult.RotationLeftORightGameObject( base_object.transform.rotation.w, KeyCode.LeftArrow, 0, -1, 0.1f);
-        rotateCapult.RotationLeftORightGameObject(base_object.transform.rotation.w, KeyCode.RightArrow, 0, 1, 0.1f);
-    }
-   
-  
-    private void ThrowBall(KeyCode key, Vector3 target, Vector3 origin, float time, GameObject throwBall)
-    {
-        Vector3 Vo = CalculateVelocity(target, origin, time);
-
-        if (Input.GetKeyDown(key))
-        {
-            //angularVelocity mierzy predkosc katowa, stopnie na sekunde
-            Vr = Vr == 0 ? -6.0f : -Vr;
-            GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, Vr);
-            var rb = throwBall.GetComponent<Rigidbody>();
-            rb.MovePosition(throwBall.transform.position + 0.1f * Vo *time);
-        }
+        throwBall.ThrowBall(KeyCode.Space,target_position.transform.position, ball_gameObject.transform.position,Time.deltaTime);
         
-    }
-    private void MoveForwardObject(float speed, GameObject gameobject, KeyCode key)
-    {
-        if (Input.GetKeyDown(key))
-        { 
-            Vector3 velocity = new Vector3(speed,0f, 0f);
-            var rb = gameobject.GetComponent<Rigidbody>();
-            rb.interpolation = RigidbodyInterpolation.Interpolate;
-            //rb.MovePosition(gameobject.transform.position + speed * velocity * Time.fixedDeltaTime);
-        }
-    }
-    private void ShootTheTarget(float speed, GameObject target,GameObject ball,Vector3 startPosition, KeyCode key)
-    {
-        if (Input.GetKeyDown(key))
-        {
-            Vector3 velocity = new Vector3(speed, 0f, 0f);
-            var rb = ball.GetComponent<Rigidbody>();
-            rb.interpolation = RigidbodyInterpolation.Interpolate;
-            rb.MovePosition(CalculateVelocity(target.transform.position,startPosition,Time.deltaTime));
-        }
+        //use keys arrow to rotate
+        rotateCapult.RotationLeftORightGameObject(mainPartCatapult.transform.rotation.w, KeyCode.LeftArrow, 0, -1, 0.1f);
+        rotateCapult.RotationLeftORightGameObject(mainPartCatapult.transform.rotation.w, KeyCode.RightArrow, 0, 1, 0.1f);
     }
     IEnumerator stopParam()
     {
         yield return new WaitForSeconds(.3f);
         GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
-
     }
 
-    Vector3 CalculateVelocity(Vector3 target, Vector3 origin, float time)
-    {
-        
-        Vector3 distance = target - origin;
-      //  Debug.Log("target : " + target.x.ToString() + ";" + target.y.ToString() + ";" + target.z.ToString());
-      // Debug.Log("origin : " + origin.x.ToString() + ";" + origin.y.ToString() + ";" + origin.z.ToString());
-        Vector3 distanceXZ = distance;
-      //  Debug.Log("distance : " + distance.x.ToString() + ";" + distance.y.ToString() +";"+ distance.z.ToString());
-        distanceXZ.y = 0f;
-
-        float Sy = distance.y;
-        float Sxz = distanceXZ.magnitude; //dlugosc vectora
-      //  Debug.Log("distanceXZ.magnitude : " + Sxz.ToString());
-        float Vxz = Sxz / (time * 10000);
-     //   Debug.Log("time : " + time.ToString());
-     //   Debug.Log("Vxz : " + Vxz.ToString());
-        float Vy = Sy / time + 0.5f * Mathf.Abs(Physics.gravity.y) * time;
-
-        Vector3 result = distanceXZ.normalized;
-        result *= Vxz;
-        result.y = Vy;
-
-     //   Debug.Log("resultat : " + result.x.ToString() + ";" + result.y.ToString() + ";" + result.z.ToString());
-        return result;
-    }
-    private Vector3 CalculteHalfDistance(Vector3 startPosition, Vector3 emdPosition, float time)
-    {
-        Vector3 result = new Vector3(0f, 0f, 0f);
-
-        return result;
-    }
+    
+    
 }
 
 
---
+++ Assets/Scripts/Main_Catapult_scripts/Catapult_ThrowningBall.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IThrowBall
+{
+    void ThrowBall(KeyCode key, Vector3 target, Vector3 origin, float time);
+}
+public class Catapult_ThrowningBall : IThrowBall
+{
+    private float Vr = 0f;
+    private Rigidbody rbBall;
+    private GameObject ball_gameObject;
+    private GameObject mainPart;
+
+    public Catapult_ThrowningBall(float vr,  GameObject ball_gameObject, GameObject mainPart)
+    {
+        Vr = vr;
+        this.ball_gameObject = ball_gameObject;
+        rbBall = this.ball_gameObject.GetComponent<Rigidbody>();
+        this.mainPart = mainPart;
+    }
+
+    public void ThrowBall(KeyCode key, Vector3 target, Vector3 origin, float time)
+    {
+        Vector3 Vo = CalculateVelocity(target, origin, time);
+
+        if (Input.GetKeyDown(key))
+        {
+            //angularVelocity mierzy predkosc katowa, stopnie na sekunde
+            Vr = Vr == 0 ? -6.0f : -Vr;
+            this.mainPart.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, Vr);
+            rbBall.MovePosition(this.ball_gameObject.transform.position + 0.1f * Vo * time);
+        }
+    }
+   private Vector3 CalculateVelocity(Vector3 target, Vector3 origin, float time)
+    {
+
+        Vector3 distance = target - origin;
+        //  Debug.Log("target : " + target.x.ToString() + ";" + target.y.ToString() + ";" + target.z.ToString());
+        // Debug.Log("origin : " + origin.x.ToString() + ";" + origin.y.ToString() + ";" + origin.z.ToString());
+        Vector3 distanceXZ = distance;
+        //  Debug.Log("distance : " + distance.x.ToString() + ";" + distance.y.ToString() +";"+ distance.z.ToString());
+        distanceXZ.y = 0f;
+
+        float Sy = distance.y;
+        float Sxz = distanceXZ.magnitude; //dlugosc vectora
+                                          //  Debug.Log("distanceXZ.magnitude : " + Sxz.ToString());
+        float Vxz = Sxz / (time * 10000);
+        //   Debug.Log("time : " + time.ToString());
+        //   Debug.Log("Vxz : " + Vxz.ToString());
+        float Vy = Sy / time + 0.5f * Mathf.Abs(Physics.gravity.y) * time;
+
+        Vector3 result = distanceXZ.normalized;
+        result *= Vxz;
+        result.y = Vy;
+
+        //   Debug.Log("resultat : " + result.x.ToString() + ";" + result.y.ToString() + ";" + result.z.ToString());
+        return result;
+    }
+    private void ShootTheTarget(float speed, GameObject target, GameObject ball, Vector3 startPosition, KeyCode key)
+    {
+        if (Input.GetKeyDown(key))
+        {
+            Vector3 velocity = new Vector3(speed, 0f, 0f);
+            var rb = ball.GetComponent<Rigidbody>();
+            rb.interpolation = RigidbodyInterpolation.Interpolate;
+            rb.MovePosition(CalculateVelocity(target.transform.position, startPosition, Time.deltaTime));
+        }
+    }
+}
